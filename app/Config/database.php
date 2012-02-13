91805e091692a9c1ebc09833ebef2962b8ed6e2b
Luca Rager <antarianetwork@gmail.com>
2012-02-13 10:34:58 +0100

    Modified .gitignore to add database.php. please come speak to me before committing anything new'

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '%host%',
+		'login' => '%login%',
+		'password' => '%password%',
+		'database' => '%database%',
+		'prefix' => '',
+		'port' => NULL
+	);
+}
