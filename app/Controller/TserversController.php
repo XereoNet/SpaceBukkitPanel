21c3db7123a0e7175378e6e6432b57524c67b4dc
JamyDev <eurostar6@gmail.com>
2012-02-15 21:26:27 +0100

    Used new per build url as of Dinnerbone: http://forums.bukkit.org/threads/introducing-dl-bukkit-org.59836/page-3#post-966179

--- app/Controller/TserversController.php
+++ app/Controller/TserversController.php
@@ -155,8 +155,8 @@ class TServersController extends AppController {
         //check if new files exist at all
         //they are either called craftbukkit.jar or craftbukkit-dev.jar
 
-        $cburl1 = 'http://dl.bukkit.org/downloads/craftbukkit/get/'.$cb.'/craftbukkit-dev.jar';
-        $cburl2 = 'http://dl.bukkit.org/downloads/craftbukkit/get/'.$cb.'/craftbukkit.jar';
+        $cburl1 = 'http://dl.bukkit.org/downloads/craftbukkit/get/build-'.$cb.'/craftbukkit-dev.jar';
+        $cburl2 = 'http://dl.bukkit.org/downloads/craftbukkit/get/build-'.$cb.'/craftbukkit.jar';
 
         if ((!(fopen($cburl1, "r"))) && (!(fopen($cburl2, "r")))) {
 
