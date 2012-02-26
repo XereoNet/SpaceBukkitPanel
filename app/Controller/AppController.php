14d10c1d1155ae0607ef9893b8bcc956d340146a
JamyDev <eurostar6@gmail.com>
2012-02-26 15:50:40 +0100

    Wrong again xD

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -279,9 +279,9 @@ class AppController extends Controller {
         }
                 
         //New variable "$usr" stores all data relative to the user, relative to the current server
-        $conditions = array("ServersUsers.user_id" => $this->Auth->user("id"));
+        $conditions = array("ServersUsers.server_id" => $current_server, "ServersUsers.user_id" => $this->Auth->user("id"));
         $usr = $this->ServersUsers->find('first', array('conditions' => $conditions));
-        $usr['AllowedServers'] = $this->ServersUsers->find('all', array('conditions' => $conditions));
+        $usr['AllowedServers'] = $this->ServersUsers->find('all', array('conditions' => array("ServersUsers.user_id" => $this->Auth->user("id"))));
 
         //If superuser, construct his "$usr" manually
 
