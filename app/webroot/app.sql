b12a25cbdf6457e7c22c0a6182acb741a09e6c0f
Jamy Timmermans <eurostar6@gmail.com>
2012-05-22 00:19:18 +0200

    Fixed admins online by adding the active table in the database

--- app/webroot/app.sql
+++ app/webroot/app.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Apr 22, 2012 at 08:55 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Machine: localhost
+-- Genereertijd: 22 Mei 2012 om 00:17
+-- Serverversie: 5.5.8
+-- PHP-Versie: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,13 +16,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `sbtemp`
+-- Database: `sbexp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `space_roles`
+-- Tabelstructuur voor tabel `space_roles`
 --
 
 CREATE TABLE IF NOT EXISTS `space_roles` (
@@ -32,50 +31,52 @@ CREATE TABLE IF NOT EXISTS `space_roles` (
   `pages` smallint(6) DEFAULT NULL,
   `global` smallint(6) DEFAULT NULL,
   `dash` smallint(6) DEFAULT NULL,
-  `users` smallint(6) DEFAULT NULL,
+  `space_users` smallint(6) DEFAULT NULL,
   `plugins` smallint(6) DEFAULT NULL,
   `worlds` smallint(6) DEFAULT NULL,
-  `servers` smallint(6) DEFAULT NULL,
-  `backups` smallint(6) DEFAULT NULL,
-  `permissions` smallint(6) DEFAULT NULL,
-  `timeline` smallint(6) DEFAULT NULL,
+  `space_servers` smallint(6) DEFAULT NULL,
   `settings` smallint(6) DEFAULT NULL,
   `fallback` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Dumping data for table `space_roles`
+-- Gegevens worden uitgevoerd voor tabel `space_roles`
 --
 
-INSERT INTO `space_roles` (`id`, `title`, `pages`, `global`, `dash`, `users`, `plugins`, `worlds`, `servers`, `backups`, `permissions`, `timeline`, `settings`, `fallback`) VALUES
-(1, 'Owner', 63, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, 0, 0),
-(2, 'Administrator', 63, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0),
-(3, 'Moderator', 63, 127, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0),
-(4, 'Viewer', 1, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 1);
+INSERT INTO `space_roles` (`id`, `title`, `pages`, `global`, `dash`, `space_users`, `plugins`, `worlds`, `space_servers`, `settings`, `fallback`) VALUES
+(1, 'Owner', 63, 0, 0, 0, 0, 0, 0, 0, 0),
+(2, 'Administrator', 63, 0, 0, 0, 0, 0, 0, NULL, 0),
+(3, 'Moderator', 7, 0, 0, 0, 0, 0, 0, NULL, 0),
+(4, 'Viewer', 1, 0, 0, 0, 0, 0, 0, NULL, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `space_servers`
+-- Tabelstructuur voor tabel `space_servers`
 --
 
 CREATE TABLE IF NOT EXISTS `space_servers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
-  `external_address` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `port1` varchar(50) DEFAULT NULL,
   `port2` varchar(50) DEFAULT NULL,
   `default_role` varchar(50) DEFAULT NULL,
+  `external_address` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `space_servers`
+--
+
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `space_servers_users`
+-- Tabelstructuur voor tabel `space_servers_users`
 --
 
 CREATE TABLE IF NOT EXISTS `space_servers_users` (
@@ -84,12 +85,17 @@ CREATE TABLE IF NOT EXISTS `space_servers_users` (
   `server_id` int(10) unsigned NOT NULL,
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `space_servers_users`
+--
+
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `space_users`
+-- Tabelstructuur voor tabel `space_users`
 --
 
 CREATE TABLE IF NOT EXISTS `space_users` (
@@ -99,12 +105,14 @@ CREATE TABLE IF NOT EXISTS `space_users` (
   `password` varchar(50) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
+  `active` varchar(50) DEFAULT NULL,
   `theme` varchar(60) DEFAULT NULL,
   `language` varchar(50) DEFAULT NULL,
   `is_super` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Gegevens worden uitgevoerd voor tabel `space_users`
+--
+
--- app/webroot/appold.sql
+++ app/webroot/appold.sql
@@ -3,21 +3,27 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 30. Jan 2012 um 20:16
--- Server Version: 5.5.16
--- PHP-Version: 5.3.8
+-- Generation Time: Apr 22, 2012 at 08:55 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Datenbank: `spacebukkit`
+-- Database: `sbtemp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `space_roles`
+-- Table structure for table `space_roles`
 --
 
 CREATE TABLE IF NOT EXISTS `space_roles` (
@@ -26,46 +32,50 @@ CREATE TABLE IF NOT EXISTS `space_roles` (
   `pages` smallint(6) DEFAULT NULL,
   `global` smallint(6) DEFAULT NULL,
   `dash` smallint(6) DEFAULT NULL,
-  `space_users` smallint(6) DEFAULT NULL,
+  `users` smallint(6) DEFAULT NULL,
   `plugins` smallint(6) DEFAULT NULL,
   `worlds` smallint(6) DEFAULT NULL,
-  `space_servers` smallint(6) DEFAULT NULL,
+  `servers` smallint(6) DEFAULT NULL,
+  `backups` smallint(6) DEFAULT NULL,
+  `permissions` smallint(6) DEFAULT NULL,
+  `timeline` smallint(6) DEFAULT NULL,
   `settings` smallint(6) DEFAULT NULL,
   `fallback` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Daten für Tabelle `space_roles`
+-- Dumping data for table `space_roles`
 --
 
-INSERT INTO `space_roles` (`id`, `title`, `pages`, `global`, `dash`, `space_users`, `plugins`, `worlds`, `space_servers`, `settings`, `fallback`) VALUES
-(1, 'Owner', 63, 0, 0, 0, 0, 0, 0, 0, 0),
-(2, 'Administrator', 63, 0, 0, 0, 0, 0, 0, NULL, 0),
-(3, 'Moderator', 7, 0, 0, 0, 0, 0, 0, NULL, 0),
-(4, 'Viewer', 1, 0, 0, 0, 0, 0, 0, NULL, 1);
+INSERT INTO `space_roles` (`id`, `title`, `pages`, `global`, `dash`, `users`, `plugins`, `worlds`, `servers`, `backups`, `permissions`, `timeline`, `settings`, `fallback`) VALUES
+(1, 'Owner', 63, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, 0, 0),
+(2, 'Administrator', 63, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0),
+(3, 'Moderator', 63, 127, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0),
+(4, 'Viewer', 1, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `space_servers`
+-- Table structure for table `space_servers`
 --
 
 CREATE TABLE IF NOT EXISTS `space_servers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
+  `external_address` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `port1` varchar(50) DEFAULT NULL,
   `port2` varchar(50) DEFAULT NULL,
   `default_role` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `space_servers_users`
+-- Table structure for table `space_servers_users`
 --
 
 CREATE TABLE IF NOT EXISTS `space_servers_users` (
@@ -74,12 +84,12 @@ CREATE TABLE IF NOT EXISTS `space_servers_users` (
   `server_id` int(10) unsigned NOT NULL,
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `space_users`
+-- Table structure for table `space_users`
 --
 
 CREATE TABLE IF NOT EXISTS `space_users` (
@@ -93,11 +103,8 @@ CREATE TABLE IF NOT EXISTS `space_users` (
   `language` varchar(50) DEFAULT NULL,
   `is_super` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
---
--- Daten für Tabelle `space_users`
---
-
-INSERT INTO `space_users` (`id`, `favourite_server`, `username`, `password`, `created`, `modified`, `theme`, `language`, `is_super`) VALUES
-(1, 24, 'super', '0f26bb253b5021af068aa43a252c7cab018172c2', '2011-12-02 01:43:58', '2012-01-29 12:47:56', 'Spacebukkit', 'eng', 1);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
