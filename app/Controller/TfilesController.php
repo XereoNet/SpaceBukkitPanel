698a0e0c4c093a3588aa94ff86b1beac6da6f8be
Jamy Timmermans <eurostar6@gmail.com>
2012-05-22 00:56:00 +0200

    stupid me, it doesnt work like that, well I guess I learned sth new :)

--- app/Controller/TfilesController.php
+++ app/Controller/TfilesController.php
@@ -149,17 +149,17 @@ class TfilesController extends AppController {
                         $filename
                     </div>
                     <div class="f-mime">
-                        $filesize __('KB'), $filemime
+                        $filesize KB, $filemime
                     </div>
                     <div class="f-btns">
 
                         $factions
 
                         <span class="button-group">
-                            <a class="button icon log" href="#">__('Edit')</a>
-                            <a class="button icon move" href="#">__('Move')</a>
-                            <a class="button icon edit" href="#">__('Rename')</a>
-                            <a class="button icon remove danger" href="#">__('Delete')</a>               
+                            <a class="button icon log" href="#">Edit</a>
+                            <a class="button icon move" href="#">Move</a>
+                            <a class="button icon edit" href="#">Rename</a>
+                            <a class="button icon remove danger" href="#">Delete</a>               
                         </span>
 
                     </div>
