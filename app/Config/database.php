433e9fb30cbe73c6b2eee60f068225d6d8bda797
Antariano <lucarager@gmail.com>
2012-07-06 17:40:12 +0200

    added database.php

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => '%type%',
+		'persistent' => false,
+		'host' => '%host%',
+		'login' => '%login%',
+		'password' => '%password%',
+		'database' => '%database%',
+		'prefix' => '',
+		'port' => NULL
+	);
+}
