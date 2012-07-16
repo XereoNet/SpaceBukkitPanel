5847fd81915752bcaea979c947c2ffe3b08ca05e
Alexander Schulz <alex@nope.bz>
2012-07-16 22:52:50 +0300

    Should fix the "Strict (2048): Declaration of User::beforeSave() should be compatible with Model::beforeSave($options = Array)" on login.

--- app/Model/User.php
+++ app/Model/User.php
@@ -30,11 +30,11 @@ class User extends AppModel {
     );
 
 
-    public function beforeSave() {
-    if (isset($this->data[$this->alias]['password'])) {
-        $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
-    }
-    return true;
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+        }
+        return true;
     }
 
     
